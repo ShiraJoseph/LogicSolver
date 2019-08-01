cff4a018ee41beafb6fd119d785bc833dae0ddd1
Shira Joseph <sjoseph@catalyte.io>
2019-08-01 16:52:25 -0400

    got header layouts to a reasonable spot

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,48 +9,61 @@ import { Tile, TileService, TileType } from '../tile.service';
 })
 export class HeaderComponent implements OnInit {
   @Input() tile: Tile;
+  isFeatureHeader = true;
   disableFeatureDelete = false;
   disableOptionDelete = false;
-
-  getDisabledDelete() {
-    return this.tile.type === TileType.LEFT_FEATURE_HEADER || this.tile.type === TileType.TOP_FEATURE_HEADER ?
-      !this.dataService.getAllowDeleteFeatures() :
-      !this.dataService.getAllowDeleteOptions();
-  }
 
   constructor(private dataService: DataService, private tileService: TileService) {
   }
 
   ngOnInit() {
+    this.isFeatureHeader = this.tile.type === TileType.TOP_FEATURE_HEADER || this.tile.type === TileType.LEFT_FEATURE_HEADER;
   }
 
-  updateFeature(event, tile: Tile) {
-    console.log(event.target.value);
-    this.dataService.setFeature(tile.objectId, event.target.value);
+  getDisabledDelete() {
+    return this.isFeatureHeader ? !this.dataService.getAllowDeleteFeatures() : !this.dataService.getAllowDeleteOptions();
+  }
+
+  updateHeader(event) {
+    if (this.isFeatureHeader) {
+      this.dataService.setFeature(this.tile.objectId, event.target.value);
+    } else {
+      this.dataService.setOption(this.tile.objectId, event.target.value);
+    }
     this.tileService.buildGrid();
-    tile.shouldShowMinus = false;
+    this.tile.shouldShowMinus = false;
   }
 
-  updateOption(event, tile: Tile) {
-    console.log(event.target.value);
-    this.dataService.setOption(tile.objectId, event.target.value);
-    this.tileService.buildGrid();
-    tile.shouldShowMinus = false;
-  }
+  // updateFeature(event) {
+  //   console.log(event.target.value);
+  //   this.dataService.setFeature(this.tile.objectId, event.target.value);
+  //   this.tileService.buildGrid();
+  //   this.tile.shouldShowMinus = false;
+  // }
+  //
+  // updateOption(event, tile: Tile) {
+  //   console.log(event.target.value);
+  //   this.dataService.setOption(tile.objectId, event.target.value);
+  //   this.tileService.buildGrid();
+  //   tile.shouldShowMinus = false;
+  // }
 
-  deleteFeature(tile: Tile) {
-    this.dataService.deleteFeature(tile.objectId);
-    if (this.dataService.features.length <= 2) {
-      this.disableFeatureDelete = true;
+  deleteHeader() {
+    if (this.isFeatureHeader) {
+      this.dataService.deleteFeature(this.tile.objectId);
+      this.disableFeatureDelete = this.dataService.features.length < 3;
+    } else {
+      this.dataService.deleteOption(this.tile.objectId);
+      this.disableOptionDelete = this.dataService.optionCount < 2;
     }
     this.tileService.buildGrid();
   }
 
-  deleteOption(tile: Tile) {
-    this.dataService.deleteOption(tile.objectId);
-    if (this.dataService.optionCount <= 1) {
-      this.disableOptionDelete = true;
-    }
-    this.tileService.buildGrid();
-  }
+  // deleteOption(tile: Tile) {
+  //   this.dataService.deleteOption(tile.objectId);
+  //   if (this.dataService.optionCount <= 1) {
+  //     this.disableOptionDelete = true;
+  //   }
+  //   this.tileService.buildGrid();
+  // }
 }
